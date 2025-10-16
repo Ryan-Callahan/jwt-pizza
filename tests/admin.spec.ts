b6667fdbb111c642dedc1493fbdb9e3ee0bf0b44
Ryan Callahan <ryan.callahan1124@gmail.com>
2025-10-16 15:57:11 -0600

    Wrote a test for listing users

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -9,5 +9,20 @@ test('admins can list users', async ({ page }) => {
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'Admin' }).click();
 
+  await expect(page.getByRole('main')).toContainText('Users');
+  await expect(page.getByRole('main')).toContainText('Name');
+  await expect(page.getByRole('main')).toContainText('Submit');
+  await page.getByRole('button', { name: '»' }).first().click();
+
+  await page.getByRole('button', { name: '«' }).first().click();
+  await page.getByRole('textbox', { name: 'Filter users' }).click();
+  await page.getByRole('textbox', { name: 'Filter users' }).fill('blah');
+  await page.getByRole('cell', { name: 'blah Submit' }).getByRole('button').click();
+  await page.getByRole('textbox', { name: 'Filter users' }).click();
+  await page.getByRole('textbox', { name: 'Filter users' }).fill('');
+  await page.getByRole('button', { name: 'Submit' }).first().click();
+});
+
+test('admin can delete users', async ({ page }) => {
 
 });
